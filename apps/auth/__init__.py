18f5c3a9941afba45fbb34ba42c423528d2f5c4b
Ishmam Hossain <37960094+ishmam-hossain@users.noreply.github.com>
2019-07-19 22:09:11 +0600

    Update __init__.py
    changed blueprint

--- apps/auth/__init__.py
+++ apps/auth/__init__.py
@@ -3,8 +3,5 @@ from .views import RegisterAPI, TestAPI
 
 
 auth_bp = Blueprint('auth', url_prefix='/auth')
-# auth_bp.add_route(LoginAPI.as_view(), '/login')
-# auth_bp.add_route(LogoutAPI.as_view(), '/logout')
 auth_bp.add_route(RegisterAPI.as_view(), '/register')
-# auth_bp.add_route(RefreshAPI.as_view(), '/refresh')
 auth_bp.add_route(TestAPI.as_view(), '/test')
